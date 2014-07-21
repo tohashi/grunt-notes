1274aecbdf8217df6ce431f3fba9de5b201389db
t93 <dots64if@gmail.com>
2014-07-21 14:02:59 +0900

    Fix bug

--- tasks/notes.js
+++ tasks/notes.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
     // TODO ignoring options
     grunt.registerMultiTask('notes', 'Enumerate annotations like todo, fixme, optimize or note', function() {
         var done = this.async();
-        var notes = new Notes(this.src || './', this.options());
+        var notes = new Notes(this.data.src || './', this.options());
 
         notes.annotate(done);
     });
